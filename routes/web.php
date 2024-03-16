152aab73d5ccc061d022e12bb45180ec30c25f5d
patrik <patkonemcok@gmail.com>
2024-03-16 22:06:46 +0100

    [22:06] - UsersController CRUD + users routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\FeedController;
 use App\Http\Controllers\ClassController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +22,16 @@ Route::get('/login', [LoginController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'index']);
 Route::get('/feed', [FeedController::class, 'index']);
 
-Route::get('/classes', [ClassController::class, 'index'])->name('classIndex');
-Route::get('/classes/create', [ClassController::class, 'create'])->name('classCreate');
-Route::post('/classes/store', [ClassController::class, 'store']);
-Route::get('/classes/edit/{id}', [ClassController::class, 'edit'])->name('classEdit');
-Route::post('/classes/update/{id}', [ClassController::class, 'update']);
-Route::get('/classes/delete/{id}', [ClassController::class, 'delete']);
+Route::get('classes/index', [ClassController::class, 'index'])->name('classIndex');
+Route::get('classes/create', [ClassController::class, 'create'])->name('classCreate');
+Route::post('classes/store', [ClassController::class, 'store']);
+Route::get('classes/edit/{id}', [ClassController::class, 'edit'])->name('classEdit');
+Route::post('classes/update/{id}', [ClassController::class, 'update']);
+Route::post('classes/delete/{id}', [ClassController::class, 'delete']);
+
+Route::get('users/index', [UsersController::class, 'index'])->name('usersIndex');
+Route::get('users/create', [UsersController::class, 'create'])->name('usersCreate');
+Route::post('users/store', [UsersController::class, 'store']);
+Route::get('users/edit/{id}', [UsersController::class, 'edit'])->name('usersEdit');
+Route::post('users/update/{id}', [UsersController::class, 'update']);
+Route::post('users/delete/{id}', [UsersController::class, 'delete']);

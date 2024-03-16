ed36b814397bccb1c3207e4c68da7503451027bc
patrik <patkonemcok@gmail.com>
2024-03-16 19:24:12 +0100

    [19:24] - Added Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/register', function () {
+    return view('register');
+});
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+Route::get('/classes', function() {
+    return view('classes');
+});

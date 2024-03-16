ce6c930ccaf3e6a624502ff33bbb59476ef85b8e
patrik <patkonemcok@gmail.com>
2024-03-16 22:15:57 +0100

    [22:15] - Added users & classes views folder + instructions inside

---
+++ resources/views/classes/index.blade.php
@@ -0,0 +1,4 @@
+{{--
+//TODO - Natiahnut master layout, pridat do folderu blady 'create', 'edit' a pokracovat podla designu
+//NOTE - Ked nevies ako sa ma nieco spravit, tak mi napis
+//LINK - Ak sa kukas ako prijebany ze co to robim za komenty tak si stiahnit tento extension https://marketplace.visualstudio.com/items?itemName=ExodiusStudios.comment-anchors

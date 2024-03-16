e5a8134f4c35be1eb3b1e60b86d8eaf0d6d4e2a9
patrik <patkonemcok@gmail.com>
2024-03-16 20:05:30 +0100

    [20:05] - Added migrations

--- database/migrations/2024_03_16_184422_create_classes_table.php
+++ database/migrations/2024_03_16_184422_create_classes_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('classes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('personal_card');
-            $table->rememberToken();
+            $table->string('class_num')->unique();
+            $table->boolean('is_open');
+            $table->string('teacher');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('classes');
     }
 };
